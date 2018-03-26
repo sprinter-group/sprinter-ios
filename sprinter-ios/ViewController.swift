412c752080599c80a7761629a8dd26a830198f5d
김재희 <jaehui327@naver.com>
2018-03-26 14:29:52 +0900

    first commit

---
+++ sprinter-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sprinter-ios
+//
+//  Created by 김재희 on 2018. 3. 26..
+//  Copyright © 2018년 김재희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
